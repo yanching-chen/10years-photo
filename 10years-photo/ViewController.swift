b3524371d2ef8d2a8af00b77616b42460b84d650
yanching.chen <yanching.chen@gmail.com>
2019-08-04 18:30:35 +0800

    Initial Commit

---
+++ 10years-photo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10years-photo
+//
+//  Created by macadmin on 2019/8/4.
+//  Copyright © 2019 macadmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
